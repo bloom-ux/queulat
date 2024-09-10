355482215b5018c45c0bce6b626b59ee239643b5
Felipe Lavín <felipe@yukei.net>
2024-09-10 11:35:02 -0300

    simplify and fix wp.media initialization

--- src/forms/elements/js/element-wp-media.js
+++ src/forms/elements/js/element-wp-media.js
@@ -1,120 +1,98 @@
-;(function($){
-	$( document ).on(
-		'click', 'button.queulat-wpmedia-upload', function( event ){
-			var $container = $( this ).closest( '.js-queulat-wp-media' ),
-			$mediaButton   = $container.find( 'button.queulat-wpmedia-upload' ),
-			$receiver      = $container.find( 'div.queulat-wpmedia-receiver' ),
-			values         = $container.data( 'wpmedia-value' ),
-			item_template  = _.template( $container.find( '.tmpl-wpmedia-item' ).html() ),
-			// Use states to filter media library
-			// @see https://wordpress.stackexchange.com/questions/343289/add-media-attachment-filter-to-custom-wp-media-frame
-			queulatState   = wp.media.controller.Library.extend(
-				{
-					defaults :  _.defaults( 
-						$.extend(
-							{
-								'id': 'queulat-state'
-							},
-							$container.data( 'wpmedia-args' )
-						), 
-						wp.media.controller.Library.prototype.defaults
-					)
-				}
-			),
-			args           = $.extend(
-				true, {
-					title: '',
-					multiple: false,
-					button: {
-						text: ''
-					},
-					state: 'queulat-state',
-					states: [ new queulatState() ]
-				}, $container.data( 'wpmedia-args' )
-			),
-			selected       = '',
-			attachments    = [],
-			file_frame;
+/* global jQuery, _ */
+(function ($) {
+	$(document)
+		.on('click', 'button.queulat-wpmedia-upload', function (event) {
+			// eslint-disable-next-line prefer-const
+			let fileFrame;
 
 			// props to @mikejolley and @hugosolar for this
 			// @see https://mikejolley.com/2012/12/21/using-the-new-wordpress-3-5-media-uploader-in-plugins/
 			event.preventDefault();
 
 			// If the media frame already exists, reopen it.
-			if ( file_frame ) {
-				file_frame.open();
+			if (fileFrame) {
+				fileFrame.open();
 				return;
 			}
 
+			const $container = $(this).closest('.js-queulat-wp-media');
+
+			const args = $.extend(
+				true,
+				{
+					title: '',
+					multiple: false,
+					button: {
+						text: '',
+					},
+				},
+				$container.data('wpmedia-args')
+			);
+
 			// Create the media frame.
-			file_frame = wp.media( args );
+			fileFrame = wp.media(args);
 
 			// When opening the media library, check and pre-select existing values
-			file_frame.on(
-				'open', function(){
-					var selection = file_frame.state().get( 'selection' );
-					$container.find( 'input.queulat-wpmedia-value' ).each(
-						function(i, obj){
-							var id     = parseInt( obj.value, 10 ),
-							attachment = wp.media.model.Attachment.get( id );
-							selection.add( attachment );
-						}
-					);
-				}
+			fileFrame.on('open', function () {
+				const selection = fileFrame.state().get('selection');
+				$container
+					.find('input.queulat-wpmedia-value')
+					.each(function (i, obj) {
+						const id = parseInt(obj.value, 10),
+							attachment = wp.media.model.Attachment.get(id);
+						selection.add(attachment);
+					});
+			});
+
+			const itemTemplate = _.template(
+				$container.find('.tmpl-wpmedia-item').html()
 			);
+			const renderItems = function (items) {
+				let selectedItems = '';
+				items.forEach(function (item) {
+					selectedItems += itemTemplate(item);
+				});
+				return selectedItems;
+			};
 
-			var renderItems = function( items ) {
-				var selected = '';
-				items.forEach(
-					function(item){
-						selected += item_template( item );
-					}
-				);
-				return selected;
-			}
-
+			const $receiver = $container.find('div.queulat-wpmedia-receiver');
 			// When an image is selected, run a callback.
-			file_frame.on(
-				'select', function() {
-					var selection = file_frame.state().get( 'selection' );
-					$receiver.html( renderItems( selection ) );
-				}
-			);
+			fileFrame.on('select', function () {
+				const selection = fileFrame.state().get('selection');
+				$receiver.html(renderItems(selection));
+			});
 
-			file_frame.open();
-		}
-	).on( 'click', 'button.queulat-wpmedia-item-remove', function( event ){
-		event.preventDefault();
-		$( this ).closest( 'div.queulat-wpmedia-item' ).fadeOut(
-			'fast', function(){
-				$(this).remove();
-			}
-		);
-	} ).ready(
-		function(){
-				$( '.js-queulat-wp-media' ).each(
-					function(){
-						var values    = $( this ).data( 'wpmedia-value' ),
-						item_template = _.template( $( this ).find( '.tmpl-wpmedia-item' ).html() ),
-						$receiver     = $( this ).find( 'div.queulat-wpmedia-receiver' ),
-						selected      = '';
-						if ( ! _.isEmpty( values ) ) {
-							values.forEach(
-								function( item ){
-									if ( item ) {
-										selected += item_template( { attributes: item } );
-									}
-								}
-							);
+			fileFrame.open();
+		})
+		.on('click', 'button.queulat-wpmedia-item-remove', function (event) {
+			event.preventDefault();
+			$(this)
+				.closest('div.queulat-wpmedia-item')
+				.fadeOut('fast', function () {
+					$(this).remove();
+				});
+		})
+		.ready(function () {
+			$('.js-queulat-wp-media').each(function () {
+				const values = $(this).data('wpmedia-value'),
+					itemTemplate = _.template(
+						$(this).find('.tmpl-wpmedia-item').html()
+					),
+					$receiver = $(this).find('div.queulat-wpmedia-receiver');
+				let selected = '';
+				if (!_.isEmpty(values)) {
+					values.forEach(function (item) {
+						if (item) {
+							selected += itemTemplate({ attributes: item });
 						}
-						$receiver.html( selected );
-						$( this ).find('.queulat-wpmedia-sortable').sortable({
-							containment: 'parent',
-							items: '.queulat-wpmedia-sortable-item',
-							forcePlaceholerSize: true
-						});
-					}
-				);
-		}
-	);
-})( jQuery );
+					});
+				}
+				$receiver.html(selected);
+				$(this).find('.queulat-wpmedia-sortable').sortable({
+					containment: 'parent',
+					items: '.queulat-wpmedia-sortable-item',
+					forcePlaceholerSize: true,
+				});
+			});
+		});
+})(jQuery);
