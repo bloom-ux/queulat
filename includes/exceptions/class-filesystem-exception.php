d9fd595d1a3d817c760524df68efadf337e8c0d3
Felipe Lavín <felipe@yukei.net>
2026-01-19 17:09:27 -0300

    added Filesystem_Exception

---
+++ includes/exceptions/class-filesystem-exception.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Exceptions from filesystem interactions
+ *
+ * @package Queulat
+ */
+
+namespace Queulat\Exceptions;
+
+use Exception;
+
+/**
+ * Exceptions from filesystem interactions
+ */
+class Filesystem_Exception extends Exception {
+
+}
